287111a8b8dc4ba6c6d0cc92cfcbfb085a962864
soumendub3 <soumendu.ana@gmail.com>
2016-06-15 00:52:15 -0500

    Google Sign In/Out fixed

--- app/src/main/java/com/smallestwearable/picosun/LoginActivity.java
+++ app/src/main/java/com/smallestwearable/picosun/LoginActivity.java
@@ -2,12 +2,15 @@ package com.smallestwearable.picosun;
 
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanFilter;
+import android.bluetooth.le.ScanSettings;
+import android.content.Context;
 import android.content.Intent;
-import android.content.IntentSender;
-import android.graphics.Bitmap;
+import android.content.pm.PackageManager;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -22,22 +25,25 @@ import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesUtil;
-import com.google.android.gms.common.Scopes;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.Scope;
-import com.google.android.gms.plus.Plus;
-import com.google.android.gms.plus.model.people.Person;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 // A login screen that offers login via Google
-public class LoginActivity extends Activity implements View.OnClickListener,
-        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
-    Integer REQUEST_ENABLE_BT = 1;
-    public BluetoothAdapter mBluetoothAdapter;
+public class LoginActivity extends Activity implements View.OnClickListener {
+        //GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener
+    private static final long SCAN_PERIOD = 5000; /* 5 seconds */
+    private static final int REQUEST_ENABLE_BT = 1;
+
+    private BluetoothAdapter mBluetoothAdapter;
+    private BluetoothLeScanner mLEScanner;
+    private ScanSettings settings;
+    private List<ScanFilter> filters;
+
 
     private static final int RC_SIGN_IN = 0;
     private static final String TAG = "LoginActivity";// Logcat tag
@@ -46,16 +52,18 @@ public class LoginActivity extends Activity implements View.OnClickListener,
     private GoogleApiClient mGoogleApiClient;
 
     private SignInButton btnSignIn;
-    private Button btnSignOut;
+    private Button btnSignOut, btnBTScan;
     private ImageView imgProfile;
-    private TextView txtName, txtEmail, mStatusTextView;
+    private TextView txtName, txtEmail;
     private LinearLayout llProfileLayout;
 
+    /*
     // A flag indicating that a PendingIntent is in progress and prevents us
     // from starting further intents.
     private boolean mIntentInProgress;
     private boolean mSignInClicked;
     private ConnectionResult mConnectionResult;
+    */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,21 +72,36 @@ public class LoginActivity extends Activity implements View.OnClickListener,
 
         btnSignIn = (SignInButton) findViewById(R.id.sign_in_button);
         btnSignOut = (Button) findViewById(R.id.sign_out_button);
+        btnBTScan = (Button) findViewById(R.id.scan_bt_button);
         imgProfile = (ImageView) findViewById(R.id.user_ProfilePic);
         txtName = (TextView) findViewById(R.id.user_Name);
         txtEmail = (TextView) findViewById(R.id.user_Email);
         llProfileLayout = (LinearLayout) findViewById(R.id.llProfile);
 
         // Button click listeners
-        btnSignIn.setOnClickListener(this);
-        btnSignOut.setOnClickListener(this);
+        findViewById(R.id.sign_in_button).setOnClickListener(this);
+        findViewById(R.id.sign_out_button).setOnClickListener(this);
+        findViewById(R.id.scan_bt_button).setOnClickListener(this);
 
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            Toast.makeText(this, R.string.ble_not_supported, Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+        // Initializes a Bluetooth adapter.  For API level 18 and above, get a reference to
+        // BluetoothAdapter through BluetoothManager.
+        final BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+
+        // Checks if Bluetooth is supported on the device.
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(this, R.string.error_bluetooth_not_supported, Toast.LENGTH_SHORT).show();
+            finish();
+        }
         // Configure sign-in to request the user's ID, email address, and basic
         // profile. ID and basic profile are included in DEFAULT_SIGN_IN
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestScopes(new Scope(Scopes.PLUS_LOGIN))
-                .requestScopes(new Scope(Scopes.PLUS_ME))
-                .requestEmail()
+                .requestProfile()
                 .build();
 
         // Build a GoogleApiClient with access to the Google Sign-In API and the
@@ -86,8 +109,9 @@ public class LoginActivity extends Activity implements View.OnClickListener,
         // ATTENTION: This "addApi(AppIndex.API)"was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
         mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
+                //.addConnectionCallbacks(this)
+                //.addOnConnectionFailedListener(this)
+                //.enableAutoManage(this, this)
                 .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
                 .addApi(AppIndex.API).build();
 
@@ -96,27 +120,6 @@ public class LoginActivity extends Activity implements View.OnClickListener,
         btnSignOut.setVisibility(View.GONE);
         llProfileLayout.setVisibility(View.GONE);
     }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        //Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
-        //startActivityForResult(signInIntent, RC_SIGN_IN);
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        if (mGoogleApiClient.isConnected()) {
-            mGoogleApiClient.disconnect();
-        }
-    }
-
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
@@ -126,6 +129,24 @@ public class LoginActivity extends Activity implements View.OnClickListener,
             case R.id.sign_out_button:
                 signOut();
                 break;
+        }
+    }
+/*
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
         }
     }
 
@@ -159,15 +180,6 @@ public class LoginActivity extends Activity implements View.OnClickListener,
         }
     }
 
-    private void signIn() {
-        if (!mGoogleApiClient.isConnecting()) {
-            Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
-            startActivityForResult(signInIntent, RC_SIGN_IN);
-            mSignInClicked = true;
-            resolveSignInError();
-        }
-    }
-
     private void resolveSignInError() {
         if (mConnectionResult.hasResolution()) {
             try {
@@ -177,50 +189,6 @@ public class LoginActivity extends Activity implements View.OnClickListener,
                 mIntentInProgress = false;
                 mGoogleApiClient.connect();
             }
-        }
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
-        if (requestCode == RC_SIGN_IN) {
-            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            handleSignInResult(result);
-        }
-
-        if (requestCode == RC_SIGN_IN) {
-            if (resultCode != RESULT_OK) {
-                mSignInClicked = false;
-            }
-            mIntentInProgress = false;
-            if (!mGoogleApiClient.isConnecting()) {
-                mGoogleApiClient.connect();
-            }
-        }
-    }
-
-    private void handleSignInResult(GoogleSignInResult result) {
-        Log.d(TAG, "handleSignInResult:" + result.isSuccess());
-        if (result.isSuccess()) {
-            // Signed in successfully, show authenticated UI.
-            GoogleSignInAccount acct = result.getSignInAccount();
-            String sFullName = acct.getDisplayName();
-            txtName.setText(sFullName);
-            String sEmail = acct.getEmail();
-            txtEmail.setText(sEmail);
-            Uri uPhoto = acct.getPhotoUrl();
-            imgProfile.setImageURI(null);
-            imgProfile.setImageURI(uPhoto);
-            Log.i("Login Info ",sFullName + sEmail);
-            //imgProfile.setImageDrawable(Drawable.createFromStream(
-            //        getContentResolver().openInputStream(uPhoto),
-            //        null));
-            updateUI(true);
-        } else {
-            // Signed out, show unauthenticated UI.
-            updateUI(false);
         }
     }
 
@@ -248,24 +216,94 @@ public class LoginActivity extends Activity implements View.OnClickListener,
             bmImage.setImageBitmap(result);
         }
     }
+*/
+    private void signIn() {
+        if (!mGoogleApiClient.isConnecting()) {
+            Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+            startActivityForResult(signInIntent, RC_SIGN_IN);
+            //mSignInClicked = true;
+            //resolveSignInError();
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_ENABLE_BT) {
+            if (resultCode == Activity.RESULT_CANCELED) {
+                //Bluetooth not enabled.
+                finish();
+                return;
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d(TAG, "handleSignInResult:" + result.isSuccess());
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            Toast.makeText(this, "User is signed in", Toast.LENGTH_LONG).show();
+            GoogleSignInAccount acct = result.getSignInAccount();
+            String sFullName = acct.getDisplayName();
+            txtName.setText(sFullName);
+            String sEmail = acct.getEmail();
+            txtEmail.setText(sEmail);
+
+            Uri uPhoto = acct.getPhotoUrl();
+            //System.out.println(uPhoto);
+            //imgProfile.setImageURI(null);
+            //imgProfile.setImageURI(uPhoto);
+            Log.i("Login Info ",sFullName + " " + sEmail);
+
+            InputStream imageStream = null;
+            try {
+                imageStream = getContentResolver().openInputStream(uPhoto);
+                System.out.println(imageStream);
+                imgProfile.setImageBitmap(BitmapFactory.decodeStream(imageStream));
+            } catch (FileNotFoundException e) {
+                Log.e("Image Display", "File not FOUND");
+            } finally {
+                if (imageStream != null) {
+                    try {
+                        imageStream.close();
+                    } catch (IOException e) {
+                        // Ignore the exception
+                    }
+                }
+            }
+            updateUI(true);
+        } else {
+            // Signed out, show unauthenticated UI.
+            updateUI(false);
+        }
+    }
 
     private void signOut() {
-        if (mGoogleApiClient.isConnected()) {
-
-            Auth.GoogleSignInApi.signOut(mGoogleApiClient);
+        //if (mGoogleApiClient.isConnected()) {
+            Log.i("SignOut", "Signing out ...");
+            //Auth.GoogleSignInApi.signOut(mGoogleApiClient);
             //Plus.AccountApi.clearDefaultAccount(mGoogleApiClient);
             mGoogleApiClient.disconnect();
-            updateUI(false);       }
+            updateUI(false);
+        //}
     }
 
     private void updateUI(boolean isSignedIn) {
         if (isSignedIn) {
             btnSignIn.setVisibility(View.GONE);
             btnSignOut.setVisibility(View.VISIBLE);
+            btnBTScan.setVisibility(View.VISIBLE);
             llProfileLayout.setVisibility(View.VISIBLE);
         } else {
             btnSignIn.setVisibility(View.VISIBLE);
             btnSignOut.setVisibility(View.GONE);
+            btnBTScan.setVisibility(View.GONE);
             llProfileLayout.setVisibility(View.GONE);
         }
     }
